55dd7f27f63795173509797046b94313c1bf8734
Kushal <gurjarkushal.18@gmail.com>
2026-03-18 20:28:55 +0530

    Adding a for loop to calculate power of 2

--- For.cpp
+++ For.cpp
@@ -10,5 +10,16 @@ int main() {
         cout<<n<<" x "<<i<<" = "<<n*i<<endl;
     }
 
+    int x;
+    cout<<"Enter the number you want in the exponent of 2: ";
+    cin>>x;
+
+    int ans = 1;
+    for(int i=1; i<=x; i++){
+        ans = ans*2;
+    }
+
+    cout<<ans;
+
     return 0;
 }
